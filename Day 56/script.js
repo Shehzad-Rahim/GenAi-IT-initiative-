86fe2aea439a745c274a3bb5e9c0f09ddd759777
Shehzad-Rahim <shehzadrahim33@gmail.com>
2024-05-07 14:42:29 +0500

    100 days of code

---
+++ Day 56/script.js
@@ -0,0 +1,18 @@
+// Type Conversion in javascript
+
+//String Conversion
+let value = true;
+alert(value);
+
+value = String(value); // value becoms string "true";
+
+//Numeric Conversion
+let strToNum = '10'
+alert(strToNum);
+
+strToNum = Number(strToNum); // strToNum becomes number 10;
+
+let strToBool = 'Yes';
+strToBool = Boolean(strToBool); // strToBool becomes boolean true
+strToBool = '';
+strToBool = Boolean(strToBool); // strToBool becomes boolean false
